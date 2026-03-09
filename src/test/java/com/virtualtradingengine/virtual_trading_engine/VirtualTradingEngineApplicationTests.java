a32bc218b965a05191c883743da9144f15f9aa0e
Darshan Virani <your-email@example.com>
2026-03-09 04:19:16 +0000

    some fixes with related to CI/CD workflow

--- src/test/java/com/virtualtradingengine/virtual_trading_engine/VirtualTradingEngineApplicationTests.java
+++ src/test/java/com/virtualtradingengine/virtual_trading_engine/VirtualTradingEngineApplicationTests.java
@@ -2,12 +2,13 @@ package com.virtualtradingengine.virtual_trading_engine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VirtualTradingEngineApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
